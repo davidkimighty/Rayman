1426fca3550aba81c094bf0233cf799032b92aa1
davidkimighty <davidkimighty@gmail.com>
2025-02-16 18:15:37 +0900

    minor changes

--- Samples~/Unlit/RandomSpawner.cs
+++ Samples~/Unlit/RandomSpawner.cs
@@ -6,11 +6,16 @@ using Random = UnityEngine.Random;
 public class RandomSpawner : MonoBehaviour
 {
     [SerializeField] private ColorShape shapePrefab;
-    [SerializeField] private int count = 500;
-    [SerializeField] private Vector3 bounds = new(10f, 10f, 10f);
+    [SerializeField] private Vector3 spawnArea = new(10f, 10f, 10f);
+    [SerializeField] private int spawnCount = 1000;
+    [SerializeField] private Vector3 minSize = new(0.1f, 0.1f, 0.1f);
+    [SerializeField] private Vector3 maxSize = new(0.3f, 0.3f, 0.3f);
     [SerializeField] private RaymarchRenderer raymarchRenderer;
     [SerializeField] private RaymarchGroup raymarchGroup;
+    [SerializeField] private int spawnPerFrame = 50;
 
+    private int currentCount = 0;
+    
     private void Start()
     {
         StartCoroutine(SpawnShapes());
@@ -19,25 +24,37 @@ public class RandomSpawner : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, bounds);
+        Gizmos.DrawWireCube(transform.position, spawnArea);
     }
 
     private IEnumerator SpawnShapes()
     {
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < spawnCount; i++)
         {
             Vector3 randomPos = new Vector3(
-                Random.Range(-bounds.x / 2f, bounds.x / 2f),
-                Random.Range(-bounds.y / 2f, bounds.y / 2f),
-                Random.Range(-bounds.z / 2f, bounds.z / 2f)
-            );
-            
+                Random.Range(-spawnArea.x / 2f, spawnArea.x / 2f),
+                Random.Range(-spawnArea.y / 2f, spawnArea.y / 2f),
+                Random.Range(-spawnArea.z / 2f, spawnArea.z / 2f));
             ColorShape shape = Instantiate(shapePrefab, transform.position + randomPos, Quaternion.identity);
+            
+            Vector3 randomSize = new Vector3(
+                Random.Range(minSize.x, maxSize.x),
+                Random.Range(minSize.y, maxSize.y),
+                Random.Range(minSize.z, maxSize.z));
+            shape.Size = randomSize;
+            
             Color randomColor = Random.ColorHSV();
             randomColor.a = shape.Color.a;
             shape.Color = randomColor;
+            
             raymarchGroup.AddEntity(shape);
-            yield return null;
+            currentCount++;
+
+            if (currentCount > spawnPerFrame)
+            {
+                currentCount = 0;
+                yield return null;                
+            }
         }
         raymarchRenderer.Setup();
     }
